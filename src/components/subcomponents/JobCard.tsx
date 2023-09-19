f04a0ee40e99bd5bb5be830016d98604571b4618
better5afe <abramczyk2704@gmail.com>
2023-09-19 22:54:15 +0200

    finished styling job cards on desktop width

--- src/components/subcomponents/JobCard.tsx
+++ src/components/subcomponents/JobCard.tsx
@@ -17,23 +17,28 @@ const JobCard: React.FC<JobCardProps> = ({
 	keywords,
 }) => {
 	return (
-		<div id={id.toString()} className={isFeatured ? 'job-card job-card--featured' : 'job-card'}>
+		<div
+			id={id.toString()}
+			className={isFeatured ? 'job-card job-card--featured' : 'job-card'}
+		>
 			<img src={img} alt="Company's logo" className='job-card__logo' />
-			<div className='job-card__overview'>
-				<h2 className='job-card__overview-company'>{company}</h2>
-				<div className='job-card__overview-badges'>
-					{isNew && <Badge text='new!' />}
-					{isFeatured && <Badge text='featured' className='featured'/>}
+			<div>
+				<div className='job-card__overview'>
+					<h2 className='job-card__overview-company'>{company}</h2>
+					<div className='job-card__overview-badges'>
+						{isNew && <Badge text='new!' />}
+						{isFeatured && <Badge text='featured' className='featured' />}
+					</div>
 				</div>
-			</div>
-			<div className='job-card__info'>
-				<h3 className='job-card__info-position'>{position}</h3>
-				<div className='job-card__info-details'>
-					<p>{postedAt}</p>
-					<span className='job-card__info-details--bullet'>∙</span>
-					<p>{contract}</p>
-					<span className='job-card__info-details--bullet'>∙</span>
-					<p>{location}</p>
+				<div className='job-card__info'>
+					<h3 className='job-card__info-position'>{position}</h3>
+					<div className='job-card__info-details'>
+						<p>{postedAt}</p>
+						<span className='job-card__info-details--bullet'>∙</span>
+						<p>{contract}</p>
+						<span className='job-card__info-details--bullet'>∙</span>
+						<p>{location}</p>
+					</div>
 				</div>
 			</div>
 			<div className='job-card__keywords'>
