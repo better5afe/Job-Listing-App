5e9be5c768e3a4cc9b3ff564e1725998f4bf0981
better5afe <abramczyk2704@gmail.com>
2023-09-21 18:13:51 +0200

    deleted unused files

--- src/components/UI/Main.tsx
+++ src/components/UI/Main.tsx
@@ -2,8 +2,6 @@ import FilterList from '../subcomponents/FilterList';
 import FilterSection from '../subcomponents/FilterSection';
 import JobList from '../subcomponents/JobList';
 
-import './Main.scss';
-
 const Main = () => {
 	return (
 		<main>
